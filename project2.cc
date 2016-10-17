8d13e8b54943b0f95d6c08eac172d0ed8b0e8793
Kelton J. Hislop <khislop@bb136-05.adit.mines.edu>
2016-10-16 21:03:05 -0600

    End of the day

--- project2.cc
+++ project2.cc
@@ -1,4 +1,5 @@
 #include "includes.h"
+#define NSERVER 9316
 
 
 
@@ -24,8 +25,81 @@ struct DNS_HEADER
     unsigned short add_count; // number of resource entries
 };
 
+//Constant sized fields of query structure
+struct QUESTION
+{
+    unsigned short qtype;
+    unsigned short qclass;
+};
 
+void ChangetoDnsNameFormat(char* dns,char* host) 
+{
+    int lock = 0 , i;
+    strcat((char*)host,".");
+     
+    for(i = 0 ; i < strlen((char*)host) ; i++) 
+    {
+        if(host[i]=='.') 
+        {
+            *dns++ = i-lock;
+            for(;lock<i;lock++) 
+            {
+                *dns++=host[lock];
+            }
+            lock++; //or lock=i+1;
+        }
+    }
+    *dns++='\0';
+}
 
+void getAddress(char* name){
+    int s;
+    struct sockaddr_in dest;
+    struct QUESTION *question = NULL;
+    struct DNS_HEADER *dns = NULL;
+    char buf[65536], *qname;
+
+    s = socket(AF_INET , SOCK_DGRAM , IPPROTO_UDP);
+    
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(53);
+    dest.sin_addr.s_addr = inet_addr("138.67.1.2");
+
+    //Set the DNS structure to standard queries
+    dns = (struct DNS_HEADER *)&buf;
+ 
+    dns->id = (unsigned short) htons(getpid());
+    dns->qr = 0; //This is a query
+    dns->opcode = 0; //This is a standard query
+    dns->aa = 0; //Not Authoritative
+    dns->tc = 0; //This message is not truncated
+    dns->rd = 1; //Recursion Desired
+    dns->ra = 0; //Recursion not available! hey we dont have it (lol)
+    dns->z = 0;
+    dns->ad = 0;
+    dns->cd = 0;
+    dns->rcode = 0;
+    dns->q_count = htons(1); //we have only 1 question
+    dns->ans_count = 0;
+    dns->auth_count = 0;
+    dns->add_count = 0;
+ 
+    //point to the query portion
+    qname =&buf[sizeof(struct DNS_HEADER)];
+ 
+    ChangetoDnsNameFormat(qname , name);
+    question =(struct QUESTION*)&buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname) + 1)]; //fill it
+ 
+    question->qtype = htons(1); 
+    question->qclass = htons(1); 
+ 
+    printf("\nSending Packet...");
+    if( sendto(s,(char*)buf,sizeof(struct DNS_HEADER) + (strlen((const char*)qname)+1) + sizeof(struct QUESTION),0,(struct sockaddr*)&dest,sizeof(dest)) < 0)
+    {
+        perror("sendto failed");
+    }
+    printf("Done");
+}
 
 
 
@@ -35,10 +109,11 @@ struct DNS_HEADER
 // ***************************************************************************
 int main(int argc, char **argv) {
 
+    char* name = "test";
 
+    getAddress(name);
     
 }
-
 
 
 
