1d5f10a9be7a2594d82eec75fa68dd7777f0c0b2
Kelton J. Hislop <khislop@bb136-05.adit.mines.edu>
2016-10-18 19:41:56 -0600

    Connected to a root server

--- project2.cc
+++ project2.cc
@@ -18,25 +18,31 @@
 // ***************************************************************************
 int main(int argc, char **argv) {
 
-    int sockfd;
-    int n;
+    int test;
+
+    int sockfd;	// Socketfd
+    int n;	
     socklen_t len;
-    char sendline[1024],recvline[1024];
-    struct sockaddr_in servaddr;
-    strcpy(sendline,"");
-    printf("\n Enter the message : ");
-    scanf("%s",sendline);
-    sockfd=socket(AF_INET,SOCK_DGRAM,0);
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family=AF_INET;
-    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    servaddr.sin_port=htons(5035);
-    connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
-    len=sizeof(servaddr);
-    sendto(sockfd,sendline,MAXLINE,0,(struct sockaddr*)&servaddr,len);
-    n=recvfrom(sockfd,recvline,MAXLINE,0,NULL,NULL);
-    recvline[n]=0;
-    printf("\n Server's Echo : %s\n\n",recvline);
+    char sendline[1024],recvline[1024]; // Buffers for sending and receiving
+    struct sockaddr_in servaddr;  //Server address
+    strcpy(sendline,"");  //Put an empty tring into sendline
+    printf("\n Enter the message : ");  //prompt user for mesage
+    scanf("%s",sendline);  //Receive user input
+    sockfd=socket(AF_INET,SOCK_DGRAM,0);  //Initialize the socketfd
+    bzero(&servaddr,sizeof(servaddr));  //Zero the server address
+    servaddr.sin_family=AF_INET;  //Specify and internet address
+    //servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");  //Set the address
+    //servaddr.sin_port=htons(5035);  //Set the port
+    servaddr.sin_addr.s_addr=inet_addr("138.67.1.2");  //Set the address
+    servaddr.sin_port=htons(53);  //Set the port
+    connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));  //Connect to the server
+    len=sizeof(servaddr);  //get size of server address
+    test = sendto(sockfd,sendline,MAXLINE,0,(struct sockaddr*)&servaddr,len);  //Send sendline through socket
+    printf("Send Result: %i\n", test);
+    n=recvfrom(sockfd,recvline,MAXLINE,0,NULL,NULL);  //receive a response
+    recvline[n]=0;  //set the end of the received data
+    printf("Return Result: %i\n", n);
+    printf("\n Server's Echo : %s\n\n",recvline);  //print received data
     return 0;
     
 }
